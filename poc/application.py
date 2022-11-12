a0c258a7d2f82886a7186336ba28a249ef614ef5
rydevops <daviruss@gmail.com>
2022-11-11 21:40:38 -0600

    Added login endpoint and test cases

--- poc/application.py
+++ poc/application.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import jwt
+
+import random
+import string
 
 app = FastAPI()
+jwt_secret = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(32))
+
+class Credentials(BaseModel):
+    username: str
+    password: str
 
 @app.get("/")
 async def read_root():
@@ -9,3 +19,36 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def get_item(item_id: int):
     return {"item_id": item_id}
+
+@app.post("/login")
+async def login(credentials: Credentials):
+    # For testing, using hardcode values
+    valid_credentials = {
+        "test": "test",
+    }
+
+    invalid_auth_exception = HTTPException(403, "Invalid username/password")
+
+    received_username = credentials.username
+    received_password = credentials.password
+
+    required_password = valid_credentials.get(received_username)
+
+    if required_password is None:
+        raise invalid_auth_exception
+
+    if received_password != required_password:
+        raise invalid_auth_exception
+        
+    jwt_payload = {
+        "username": received_username,
+    }
+
+    return { "token": encode_jwt(jwt_payload, jwt_secret) }
+       
+
+def encode_jwt(payload, secret):
+    return jwt.encode(payload, secret, "HS256")
+
+def decode_jwt(payload, secret):
+    return jwt.decode(payload, secret, "HS256")
--- tests/test_application.py
+++ tests/test_application.py
@@ -1,8 +1,11 @@
+import random
+import string
+
 from fastapi.testclient import TestClient
 
-from poc.application import app
+from poc import application
 
-client = TestClient(app)
+client = TestClient(application.app)
 
 def test_root_get():
     response = client.get("/")
@@ -16,5 +19,36 @@ def test_get_item():
     assert response.status_code == 200
     assert response.json() == {"item_id": item_id}
 
+def test_encode_decode_jwt():
+    payload = {"username": "test"}
+    encoded_jwt = application.encode_jwt(payload, application.jwt_secret)
+    decoded_jwt = application.decode_jwt(encoded_jwt, application.jwt_secret)
+    assert payload == decoded_jwt
 
+def test_valid_login():
+    auth_payload = {
+        "username": "test",
+        "password": "test",
+    }
 
+    jwt_payload = {
+        "username": "test",
+    }
+
+    response = client.post("/login", json=auth_payload)
+    token = response.json().get('token')
+
+    assert response.status_code == 200
+    assert token != None
+    assert jwt_payload == application.decode_jwt(token, application.jwt_secret)
+
+def test_invalid_login():
+    auth_payload = {
+        "username": "test1",
+        "password": "test1",
+    }
+
+    response = client.post("/login", json=auth_payload)
+
+    assert response.status_code == 403
+    assert response.json().get('detail') == 'Invalid username/password'
